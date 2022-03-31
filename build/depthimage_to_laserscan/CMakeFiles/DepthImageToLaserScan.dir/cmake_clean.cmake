dc8d162f5eba4518786b399deaabdc3fa985dca6
seunggeon <tmdrjs9555@naver.com>
2022-03-31 12:04:17 +0900

    realsense object-detection github add

---
+++ build/depthimage_to_laserscan/CMakeFiles/DepthImageToLaserScan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DepthImageToLaserScan.dir/src/DepthImageToLaserScan.cpp.o"
+  "/home/kw-cobot/wego_ws/auto-driving-project/devel/lib/libDepthImageToLaserScan.pdb"
+  "/home/kw-cobot/wego_ws/auto-driving-project/devel/lib/libDepthImageToLaserScan.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DepthImageToLaserScan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/depthimage_to_laserscan/CMakeFiles/DepthImageToLaserScanNodelet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DepthImageToLaserScanNodelet.dir/src/DepthImageToLaserScanNodelet.cpp.o"
+  "/home/kw-cobot/wego_ws/auto-driving-project/devel/lib/libDepthImageToLaserScanNodelet.pdb"
+  "/home/kw-cobot/wego_ws/auto-driving-project/devel/lib/libDepthImageToLaserScanNodelet.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DepthImageToLaserScanNodelet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
